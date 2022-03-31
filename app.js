79593de1fe99f22b0f6b3a3223ac644e05adb71c
hypeneutron <surachat2014@gmail.com>
2022-03-31 18:37:17 +0700

    fix view 1

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ dotenv.config();
 // const date = require("./date");
 
 const app = express();
+app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
