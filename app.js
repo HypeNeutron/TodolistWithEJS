0b6a42e6b18c3bcad31d7d8ba8a7e6cfb13395e5
hypeneutron <surachat2014@gmail.com>
2022-03-31 16:33:00 +0700

    fix time out

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./database");
 const { ObjectId } = require("mongodb");
 const _ = require("lodash");
-const date = require("./date");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
+// const date = require("./date");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -12,6 +14,64 @@ app.use(express.static("public"));
 
 const dbName = "todosListDB";
 let initialItems = [{ name: "Fruit" }, { name: "Buy food" }];
+
+const uri = `mongodb+srv://admin-hypeneutron:${process.env.API_PASS}@cluster0.hq7q7.mongodb.net/todoListDB`;
+
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
+});
+
+const validator = {
+   $and: [{ name: { $regex: /\w/i } }],
+   $jsonSchema: {
+      bsonType: "object",
+      required: ["name"],
+      properties: {
+         name: {
+            bsonType: "string",
+            description: "name be a string and is required",
+         },
+      },
+   },
+};
+
+async function conn(db) {
+   try {
+      client.connect();
+      return client.db(db);
+   } catch (e) {
+      console.error(e);
+   }
+}
+
+async function createCollection(dbn, colName) {
+   const connectDB = await conn(dbn).catch(console.error);
+   const collections = await connectDB
+      .listCollections({ name: colName }, { nameOnly: true })
+      .toArray();
+   if (collections.length) {
+      client.close();
+      return;
+   } else {
+      await connectDB.createCollection(colName, {
+         validator,
+         validationAction: "error",
+      });
+      client.close();
+   }
+}
+
+async function SLcollectName(dbn, colName) {
+   try {
+      await client.connect();
+      const connectDB = client.db(dbn);
+      return connectDB.collection(colName);
+   } catch (e) {
+      console.error(e);
+   }
+}
 
 // listen server-----------------------------------
 app.listen(process.env.PORT || 3000);
@@ -22,7 +82,7 @@ app.get("/about", function (_, res) {
 
 // get index request ejs-------------------------
 app.get("/", async function (_, res) {
-   const collect = await db.SLcollectName(dbName, "todosList");
+   const collect = await SLcollectName(dbName, "todosList");
    const all = collect.find();
    const result = await all.toArray();
    if (result.length === 0) {
@@ -30,7 +90,7 @@ app.get("/", async function (_, res) {
       res.redirect("/");
    } else {
       res.render("pages/todoList", {
-         listTitle: date.getDate(),
+         listTitle: "Today",
          newListItems: result,
       });
    }
@@ -40,9 +100,9 @@ app.get("/", async function (_, res) {
 app.get("/:paramName", async function (req, res) {
    const capParam = _.capitalize(req.params.paramName);
 
-   await db.createCollection(dbName, capParam);
+   await createCollection(dbName, capParam);
 
-   const collect = await db.SLcollectName(dbName, capParam);
+   const collect = await SLcollectName(dbName, capParam);
    const all = collect.find();
    const result = await all.toArray();
    if (result.length === 0) {
@@ -72,7 +132,7 @@ app.post("/:paramName", async function (req, res) {
    const capParam = _.capitalize(req.params.paramName);
    const { link, paramName } = linkParam(capParam);
 
-   const collect = await db.SLcollectName(dbName, paramName);
+   const collect = await SLcollectName(dbName, paramName);
    try {
       collect.insertOne({ name: _.capitalize(req.body.addList) });
       res.redirect(link);
@@ -88,7 +148,7 @@ app.post("/delete/:paramName", async function (req, res) {
    const checkedID = req.body.done;
    const { link, paramName } = linkParam(capParam);
 
-   const collect = await db.SLcollectName(dbName, paramName);
+   const collect = await SLcollectName(dbName, paramName);
    collect.findOneAndDelete({ _id: ObjectId(checkedID) }, function (err) {
       if (err) {
          throw new Error(error);
